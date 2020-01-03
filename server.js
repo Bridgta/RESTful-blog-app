794ebea166de5c5ed81189f62f187ab8a44dee63
Bridget Asser <bridgetasser@gmail.com>
2020-01-02 23:35:58 -0500

    show and create routes tested with refactored styles ejs templating

--- server.js
+++ server.js
@@ -45,9 +45,20 @@ app.post("/blogs", function(req, res) {
   //create blog
   Blog.create(req.body.blog, function(err, newBlog) {
     if (err) {
-      console.log("new");
-    } else {
       res.redirect("/blogs");
+    } else {
+      res.render("show", { blog: foundBlog });
+    }
+  });
+});
+
+//SHOW ROUTE
+app.get("/blogs/:id", function(req, res) {
+  Blog.findById(req.params.id, function(err, foundBlog) {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.render("show", { blog: foundBlog });
     }
   });
 });
