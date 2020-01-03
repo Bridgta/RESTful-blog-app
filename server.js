e4d7b34b64853596dfa1d92e0b3fcdcb67562ae9
Bridget Asser <bridgetasser@gmail.com>
2020-01-03 01:50:43 -0500

    delete route with aligning form refactor

--- server.js
+++ server.js
@@ -93,6 +93,18 @@ app.put("/blogs/:id", function(req, res) {
   });
 });
 
+//DELTE ROUTE
+
+app.delete("/blogs/:id", function(req, res) {
+  Blog.findByIdAndRemove(req.params.id, function(err) {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.redirect("/blogs");
+    }
+  });
+});
+
 app.listen(3000, function() {
   console.log("RESTful app is running on server 3000");
 });
