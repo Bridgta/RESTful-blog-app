93782b7ad37be12ca2e26bdafda2eb67fe02b92e
Bridget Asser <bridgetasser@gmail.com>
2020-01-02 23:14:35 -0500

    setting up init routes and semantic ui styled pages

---
+++ server.js
@@ -0,0 +1,65 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+app = express();
+
+//APP CONFIG
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// MONGOOSE/MODEL CONFIG
+var blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: { type: Date, default: Date.now }
+});
+var Blog = mongoose.model("Blog", blogSchema);
+
+//RESTFUL ROUTES
+app.get("/", function(req, res) {
+  res.redirect("/blogs");
+});
+
+//INDEX ROUTE
+app.get("/blogs", function(req, res) {
+  Blog.find({}, function(err, blogs) {
+    if (err) {
+      console.log("Error");
+    } else {
+      res.render("index", { blogs: blogs });
+    }
+  });
+});
+
+//NEW ROUTE
+app.get("/blogs/new", function(req, res) {
+  res.render("new");
+});
+
+//CREATE ROUTE
+
+app.post("/blogs", function(req, res) {
+  //create blog
+  Blog.create(req.body.blog, function(err, newBlog) {
+    if (err) {
+      console.log("new");
+    } else {
+      res.redirect("/blogs");
+    }
+  });
+});
+
+// title
+
+// image
+
+// body
+
+// created
+
+app.listen(3000, function() {
+  console.log("RESTful app is running on server 3000");
+});
