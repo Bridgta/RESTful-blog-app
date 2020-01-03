55127c3ecc3c134085e0615b83f6680cca224bf5
Bridget Asser <bridgetasser@gmail.com>
2020-01-03 01:00:47 -0500

    update route added with method override t0 enable POSt req from an html form

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var methodOverride = require("method-override");
+
 app = express();
 
 //APP CONFIG
@@ -8,6 +10,7 @@ mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 // MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
@@ -63,13 +66,32 @@ app.get("/blogs/:id", function(req, res) {
   });
 });
 
-// title
+//EDIT ROUTE
 
-// image
+app.get("/blogs/:id/edit", function(req, res) {
+  Blog.findById(req.params.id, function(err, foundBlog) {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.render("edit", { blog: foundBlog });
+    }
+  });
+});
 
-// body
+//UPDATE ROUTE
 
-// created
+app.put("/blogs/:id", function(req, res) {
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(
+    err,
+    updatedBlog
+  ) {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.redirect("/blogs/" + req.params.id);
+    }
+  });
+});
 
 app.listen(3000, function() {
   console.log("RESTful app is running on server 3000");
